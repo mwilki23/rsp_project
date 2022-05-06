8fab9143a1b33648418c24fd5a5a1172bddc4e74
bworono <bworono@gmail.com>
2022-05-06 00:23:26 -0400

    turtlebot_target_follow can now detect distance and rotation of fiducial

--- turtlebot_target_follow/include/turtlebot_target_follow/target_follow.hpp
+++ turtlebot_target_follow/include/turtlebot_target_follow/target_follow.hpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <fiducial_msgs/FiducialTransformArray.h>
 #include <fiducial_msgs/FiducialTransform.h>
 
 class target_follow{
@@ -11,12 +12,20 @@ class target_follow{
         ros::Publisher pub_wheelcmd;
         ros::Rate rate;
 
+	//fiducial_msgs::FiducialTransformArray empty_tf;
+
         double x_dist; 
         double z_dist; 
+	double x_quat; 
+        double y_quat; 
+        double z_quat; 
+        double w_quat; 
+	
+	double bot_to_fid_yaw;
 
     public:
         target_follow(ros::NodeHandle& nh);
         ~target_follow(){}
 
-        void callback_artf(const fiducial_msgs::FiducialTransform& artf);
+        void callback_artf(const fiducial_msgs::FiducialTransformArray& artf);
 };
--- turtlebot_target_follow/src/target_follow.cpp
+++ turtlebot_target_follow/src/target_follow.cpp
@@ -1,25 +1,54 @@
 #include <turtlebot_target_follow/target_follow.hpp>
 
 target_follow::target_follow(ros::NodeHandle& nh): nh(nh),rate(10){
-    sub_artf = nh.subscribe("fiducial_transforms",10,&target_follow::callback_artf, this);
+    sub_artf = nh.subscribe("/fiducial_transforms",10,&target_follow::callback_artf, this);
     pub_wheelcmd = nh.advertise<geometry_msgs::Twist>("cmd_vel",10);
 }
 
-void target_follow::callback_artf(const fiducial_msgs::FiducialTransform& artf){
+void target_follow::callback_artf(const fiducial_msgs::FiducialTransformArray& artf){
    // std::cout<<"test"<<std::endl;
-    x_dist = artf.transform.translation.x; //forward-backward
-    z_dist = artf.transform.translation.z; //left-right
-    //y distance is up in the air so not relevant
-    //double z_rot = artf.transform.translation.x;
-    geometry_msgs::Twist wheelcmd;
-    if (z_dist > 1.0){
-        wheelcmd.angular.z = 0.1;
-    }
-    else{
-        wheelcmd.angular.z = 0.0;
-    }
+    if(artf.transforms.empty()){
 
-    //wheelcmd.angular.z = 0.01;
-    pub_wheelcmd.publish(wheelcmd);
+	std::cout<<"No Fiducials to be found!"<<std::endl;
+    
+    }else{
+
+        // Note: +z is coming out of raspicam's lens
+	//
+	//           [o]--------> +x
+        //           /|
+	//          / |
+	//         /  |
+        //        /   |
+	//       /    |
+	// +z-> L     V <- +y
+
+
+	x_dist = artf.transforms[0].transform.translation.x; //forward-backward
+	z_dist = artf.transforms[0].transform.translation.z; //left-right
+	//y distance is up in the air so not relevant
+
+	
+  	x_quat = artf.transforms[0].transform.rotation.x; 
+        y_quat = artf.transforms[0].transform.rotation.y;
+        z_quat = artf.transforms[0].transform.rotation.z;
+        w_quat = artf.transforms[0].transform.rotation.w;
+
+	bot_to_fid_yaw = asin(2*x_quat*y_quat + 2*z_quat*w_quat); // found this online
+
+	std::cout<<"bot_to_fid_yaw = " << bot_to_fid_yaw <<std::endl;
+
+
+	geometry_msgs::Twist wheelcmd;
+	if (z_dist > 1.0){
+		//wheelcmd.angular.z = 0.1;
+		std::cout << "z_dist > 1" << std::endl;
+	}else{
+		//wheelcmd.angular.z = -0.1;
+		std::cout << "z_dist < 1" << std::endl;
+	}
+
+    }
+    //pub_wheelcmd.publish(wheelcmd);
 }
 
