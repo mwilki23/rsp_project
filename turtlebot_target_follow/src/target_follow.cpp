eba3aa73ded968898711ed94c2e37570aba79c46
bworono <bworono@gmail.com>
2022-05-09 20:43:37 -0400

    launch files and tracking lost tag

--- turtlebot_target_follow/src/target_follow.cpp
+++ turtlebot_target_follow/src/target_follow.cpp
@@ -7,30 +7,24 @@ target_follow::target_follow(ros::NodeHandle& nh): nh(nh),rate(10){
 }
 
 void target_follow::callback_artf(const fiducial_msgs::FiducialTransformArray& artf){
-   // std::cout<<"test"<<std::endl;
+   std::cout << std::setprecision(2) << std::fixed;
     if(artf.transforms.empty()){
-
 	std::cout<<"No Fiducials to be found!"<<std::endl;
-	wheelcmd.angular.z = 0.0;
 	wheelcmd.linear.x =  0.0;
-    
-    }else{
+	
+	double sgn_latshift = abs(x_dist)/(x_dist + 0.0001); //get lateral shift direction btwn bot and tag and no divide by zero error
+	//double sgn_rotshift = abs(y_rot)/(y_rot + 0.0001); //get lateral shift direction btwn bot and tag and no divide by zero error
+	wheelcmd.angular.z = -1.0*sgn_latshift*0.10; //rotates slowly to search for tag
 
-        // Note: +z is coming out of raspicam's lens
-	//
-	//           [o]--------> +x
-        //           /|
-	//          / |
-	//         /  |
-        //        /   |
-	//       /    |
-	// +z-> L     V <- +y
+    
+    }
+   else{
 
 
 	x_dist = artf.transforms[0].transform.translation.x; //forward-backward
 	z_dist = artf.transforms[0].transform.translation.z; //left-right
 	//y distance is up in the air so not relevant
-
+	std::cout<<"[x_dist z_dist] = "<< x_dist << " " << z_dist << std::endl;
 	
   	x_quat = artf.transforms[0].transform.rotation.x; 
         y_quat = artf.transforms[0].transform.rotation.y;
@@ -46,12 +40,13 @@ void target_follow::callback_artf(const fiducial_msgs::FiducialTransformArray& a
 	
 	z_rot = atan2((2*(w_quat*z_quat + x_quat*y_quat)),(1-2*(y_quat*y_quat + z_quat*z_quat)));
 	
-	std::cout << std::setprecision(2) << std::fixed;
+	
 	std::cout<<"[x_rot y_rot z_rot] = "<< x_rot << " " << y_rot << " " << z_rot << std::endl;
 
 
-	if (abs(y_rot) > 0.08){
-		wheelcmd.angular.z = -0.3*y_rot;
+	if (abs(y_rot) > 0.08) {
+		if(abs(x_dist)/abs(z_dist) < 0.4){wheelcmd.angular.z = -0.3*y_rot;}
+		else { wheelcmd.angular.z = 0.0;}
 	}
 	else{ wheelcmd.angular.z = 0.0;}
 	if (z_dist > 1.0){
